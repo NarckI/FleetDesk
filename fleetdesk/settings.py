e0fd171b7e39af3f56e0270d20b070fc3f2d0b12
Cain5YD <torresjhonrobert671@gmail.com>
2026-04-24 22:26:08 +0800

    fixed-sidebar

--- fleetdesk/settings.py
+++ fleetdesk/settings.py
@@ -77,8 +77,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'fleetdesk',
-        'USER': 'clyde',
-        'PASSWORD': '1234',
+        'USER': 'postgres',
+        'PASSWORD': 'Jobert2883',
         'HOST': 'localhost',
         'PORT': '5432',
     }
