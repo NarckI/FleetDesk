8fa509fe897e2d1a9a97d989131ba08db62a2ccf
NarckI <classacc7272@gmail.com>
2026-04-27 21:25:35 +0800

    Added Edit driver details Modal

--- fleet/views.py
+++ fleet/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
+from django.http import JsonResponse
 from django.contrib import messages
 from django.db.models import Q
 from datetime import date
